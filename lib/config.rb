8bd3cb6b70414948b7c41a05f7fb17fc1b795bbd
Poga Po <poga.bahamut@gmail.com>
2010-11-24 11:46:23 +0800

    fix syntax error

--- lib/config.rb
+++ lib/config.rb
@@ -1,10 +1,11 @@
 OPTIONS =
   if defined?(Rails.env)
-    raw_config = File.read(Rails.root.join("config", "apns4r.yml")
+    raw_config = File.read(Rails.root.join("config", "apns4r.yml"))
     parsed_config = ERB.new(raw_config).result
     YAML.load(parsed_config)[Rails.env].symbolize_keys
   else
     require 'erb'
+    require 'yaml'
     raw_config = File.read(File.expand_path(File.dirname(__FILE__)) + "/../apns4r.yml")
     parsed_config = ERB.new(raw_config).result
     YAML.load(parsed_config).inject({}) do |options, (key, value)|
